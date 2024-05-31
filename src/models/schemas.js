a26e5c829011f1bc8d061305619c3c64d050a96a
Nathalia-Soares <nathalia.oliveira24@fatec.sp.gov.br>
2024-05-31 18:38:57 -0300

    Criação de controller e CRUD completo de Usuario

---
+++ src/models/schemas.js
@@ -0,0 +1,131 @@
+const db = require('./db');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AuthSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    senha: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String,
+        required: true
+    }
+}, { collection: 'Auth' });
+
+const UsuarioSchema = new Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    img_url: {
+        type: String,
+        required: false
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    ra: {
+        type: Number,
+        required: true
+    },
+    curso: {
+        type: String,
+        required: true
+    },
+    score: {
+        type: Number,
+        required: false
+    },
+    tipo_usuario: {
+        type: String,
+        enum: ['MOTORISTA', 'PASSAGEIRO'],
+        required: true
+    },
+    veiculos: [{
+        modelo: {
+            type: String,
+            required: false
+        },
+        cor: {
+            type: String,
+            required: false
+        },
+        placa: {
+            type: String,
+            required: false
+        }
+    }]
+}, { collection: 'Usuario' });
+
+const ViagemSchema = new Schema({
+    status: {
+        type: String,
+        enum: ['PENDENTE', 'EM_ANDAMENTO', 'CONCLUIDA', 'CANCELADA'],
+        required: true
+    },
+    local_partida: {
+        type: String,
+        required: true
+    },
+    destino: {
+        type: String,
+        required: true
+    },
+    data: {
+        type: Date,
+        required: true
+    },
+    usuarios: [{
+        nome: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        tipo_usuario: {
+            type: String,
+            enum: ['MOTORISTA', 'PASSAGEIRO'],
+            required: true
+        },
+    }],
+    pagamento: [{
+        metodo: {
+            type: String,
+            enum: ['NULL', 'PIX', 'CARTAO_CREDITO'],
+            required: true
+        },
+        valor: {
+            type: Number,
+            required: true
+        },
+    }],
+    avaliacao: [{
+        nota: {
+            type: Number,
+            required: true
+        },
+        feedback: {
+            type: String,
+            required: true
+        }
+    }],
+}, { collection: 'Viagem' });
+
+
+const Auth = mongoose.model('Auth', AuthSchema);
+const Usuario = mongoose.model('Usuario', UsuarioSchema);
+const Viagem = mongoose.model('Viagem', ViagemSchema);
+
+module.exports = {
+    Auth,
+    Usuario,
+    Viagem
+}
