ecea0ed58e8afed5b349badf8e8904fd51ec7baa
Nathalia-Soares <nathalia.oliveira24@fatec.sp.gov.br>
2024-09-05 21:43:02 -0300

    Refactoring Swagger docs

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,32 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const AuthService = require('./../services/AuthService');
+const authSwaggerDocument = require('../swagger/auth-swagger.json');
 
-// cadastrar login
-/**
- * @swagger
- * /cadastro:
- *      post:
- *          tags:
- *            - Auth
- *          description: Cadastra um novo login
- *          parameters:
- *            - name: email
- *              description: Email do usuário
- *              in: formData
- *              required: true
- *              type: string
- *            - name: senha
- *              description: Senha do usuário
- *              in: formData
- *              required: true
- *              type: string
- *          responses:
- *              201:
- *                  description: Login criado com sucesso
- *              500:
- *                  description: Erro ao criar login 
- */
 router.post('/cadastro', async (req, res) => {
     try {
         const token = await AuthService.registro(req.body.email, req.body.senha);
@@ -36,31 +12,6 @@ router.post('/cadastro', async (req, res) => {
     }
 });
 
-// fazer login (encriptar senha e retornar token)
-/**
- * @swagger
- *  /auth:
- *      post:
- *          tags:
- *            - Auth
- *          description: Faz login
- *          parameters:
- *            - name: email
- *              description: Email do usuário
- *              in: formData
- *              required: true
- *              type: string
- *            - name: senha
- *              description: Senha do usuário
- *              in: formData
- *              required: true
- *              type: string
- *          responses:
- *              200:
- *                  description: Login realizado com sucesso
- *              500:
- *                  description: Erro ao fazer login
- */
 router.post('/auth', async (req, res) => {
     try {
         const { email, senha } = req.body;
@@ -71,36 +22,6 @@ router.post('/auth', async (req, res) => {
     }
 });
 
-// alterar senha (encriptar nova senha)
-/**
- * @swagger
- *  /alterar_senha:
- *      post:
- *          tags:
- *            - Auth
- *          description: Altera a senha do usuário
- *          parameters:
- *            - name: email
- *              description: Email do usuário
- *              in: formData
- *              required: true
- *              type: string
- *            - name: senha_atual
- *              description: Senha atual do usuário
- *              in: formData
- *              required: true
- *              type: string
- *            - name: nova_senha
- *              description: Nova senha do usuário
- *              in: formData
- *              required: true
- *              type: string
- *          responses:
- *              200:
- *                  description: Senha alterada com sucesso
- *              500:
- *                  description: Erro ao alterar senha
- */
 router.post('/alterar_senha', async (req, res) => {
     try {
         await AuthService.alterarSenha(req.body.email, req.body.senha_atual, req.body.nova_senha);
@@ -110,26 +31,6 @@ router.post('/alterar_senha', async (req, res) => {
     }
 });
 
-// fazer logout (remover token)
-/**
- * @swagger
- *  /logout:
- *      delete:
- *          tags:
- *            - Auth
- *          description: Faz logout
- *          parameters:
- *            - name: email
- *              description: Email do usuário
- *              in: formData
- *              required: true
- *              type: string
- *          responses:
- *              200:
- *                  description: Logout realizado com sucesso
- *              500:
- *                  description: Erro ao fazer logout
- */
 router.delete('/logout', async (req, res) => {
     try {
         await AuthService.logout(req.body.email);
--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,57 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const UsuarioService = require('./../services/UsuarioService');
+const usuarioSwaggerDocument = require('../swagger/usuario-swagger.json');
 
 const Usuario = require('../models/schemas').Usuario;
 
-// create
-/**
- * @swagger
- * /usuarios/criar_usuario:
- *   post:
- *     tags:
- *      - Usuario
- *     description: Cria um novo usuário
- *     parameters:
- *       - name: nome
- *         description: Nome do usuário
- *         in: formData
- *         required: true
- *         type: string
- *       - name: email
- *         description: Email do usuário
- *         in: formData
- *         required: true
- *         type: string
- *       - name: ra
- *         description: RA do usuário
- *         in: formData
- *         required: true
- *         type: string
- *       - name: curso
- *         description: Curso do usuário
- *         in: formData
- *         required: true
- *         type: string
- *       - name: score
- *         description: Score do usuário
- *         in: formData
- *         required: true
- *         type: number
- *       - name: tipo_usuario
- *         description: Tipo do usuário
- *         in: formData
- *         required: true
- *         type: string
- *       - name: veiculos
- *         description: Veículos do usuário
- *         in: formData
- *         required: true
- *         type: array
- *     responses:
- *       201:
- *         description: Usuário criado com sucesso
- */
 router.post('/criar_usuario', async (req, res) => {
     try {
         const { nome, email, ra, curso, score, tipo_usuario, veiculos } = req.body;
@@ -63,29 +16,6 @@ router.post('/criar_usuario', async (req, res) => {
     }
 });
 
-// Upload de imagem
-/**
- * @swagger
- * /usuarios/usuario/{id}/upload:
- *   post:
- *     tags:
- *       - Usuario
- *     description: Faz o upload de uma imagem para o usuário
- *     parameters:
- *       - name: id
- *         description: ID do usuário
- *         in: path
- *         required: true
- *         type: string
- *       - name: imagem
- *         description: Imagem do usuário
- *         in: formData
- *         required: true
- *         type: file
- *     responses:
- *       200:
- *         description: Imagem enviada com sucesso
- */
 router.post('/usuario/:id/upload', async (req, res) => {
     try {
         const dir = await UsuarioService.uploadImagem(req, res);
@@ -95,23 +25,6 @@ router.post('/usuario/:id/upload', async (req, res) => {
     }
 });
 
-// find all
-/**
- * @swagger
- * /usuarios:
- *  get:
- *      tags:
- *        - Usuario
- *      description: Retorna todos os usuários
- *      responses:
- *          200:
- *              description: Usuários encontrados
- *          500:
- *              description: Erro ao buscar usuários
- *          404:
- *              description: Usuários não encontrados
- * 
- */
 router.get('/', async (req, res) => {
     try {
         const usuarios = await Usuario.find();
@@ -121,28 +34,6 @@ router.get('/', async (req, res) => {
     }
 });
 
-//find by id
-/**
- * @swagger
- *  /usuarios/usuario/{id}:
- *      get:
- *          tags:
- *           - Usuario
- *          description: Retorna um usuário pelo ID
- *          parameters:
- *            - name: id
- *              description: ID do usuário
- *              in: path
- *              required: true
- *              type: string
- *          responses:
- *              200:
- *                  description: Usuário encontrado
- *              500:
- *                  description: Erro ao buscar usuário
- *              404:
- *                  description: Usuário não encontrado
- */
 router.get('/usuario/:id', async (req, res) => {
     try {
         const usuario = await Usuario.findById(req.params.id);
@@ -156,28 +47,6 @@ router.get('/usuario/:id', async (req, res) => {
     }
 });
 
-//find by email
-/**
- * @swagger
- *  /usuarios/usuario_email:
- *      post:
- *          tags:
- *            - Usuario
- *          description: Retorna um usuário pelo email
- *          parameters:
- *            - name: email
- *              description: Email do usuário
- *              in: path
- *              required: true
- *              type: string
- *          responses:
- *              200:
- *                  description: Usuário encontrado
- *              500:
- *                  description: Erro ao buscar usuário
- *              404:
- *                  description: Usuário não encontrado
- */
 router.post('/usuario_email', async (req, res) => {
     try {
         const usuario = await Usuario.findOne({ email: req.body.email });
@@ -191,64 +60,6 @@ router.post('/usuario_email', async (req, res) => {
     }
 });
 
-// update
-/**
- * @swagger
- *  /usuarios/editar_usuario/{id}:
- *      put:
- *          tags:
- *            - Usuario
- *          description: Atualiza um usuário pelo ID
- *          parameters:
- *            - name: id
- *              description: ID do usuário
- *              in: path
- *              required: true
- *              type: string
- *            - name: nome
- *              description: Nome do usuário
- *              in: formData
- *              required: false
- *              type: string
- *            - name: email
- *              description: Email do usuário
- *              in: formData
- *              required: false
- *              type: string
- *            - name: ra
- *              description: RA do usuário
- *              in: formData
- *              required: false
- *              type: string
- *            - name: curso
- *              description: Curso do usuário
- *              in: formData
- *              required: false
- *              type: string
- *            - name: score
- *              description: Score do usuário
- *              in: formData
- *              required: false
- *              type: number
- *            - name: tipo_usuario
- *              description: Tipo do usuário
- *              in: formData
- *              required: false
- *              type: string
- *            - name: veiculos
- *              description: Veículos do usuário
- *              in: formData
- *              required: false
- *              type: array
- *          responses:
- *              200:
- *                  description: Usuário atualizado
- *              500:
- *                  description: Erro ao atualizar usuário
- *              404:
- *                  description: Usuário não encontrado
- * 
- */
 router.put('/editar_usuario/:id', async (req, res) => {
     try {
         const { nome, email, ra, curso, score, tipo_usuario, veiculos } = req.body;
@@ -263,28 +74,6 @@ router.put('/editar_usuario/:id', async (req, res) => {
     }
 });
 
-// delete
-/**
- * @swagger
- *  /usuarios/deletar_usuario/{id}:
- *      delete:
- *          tags:
- *              - Usuario
- *          description: Deleta um usuário pelo ID
- *          parameters:
- *            - name: id
- *              description: ID do usuário
- *              in: path
- *              required: true
- *              type: string
- *          responses:
- *              200:
- *                  description: Usuário deletado
- *              500:
- *                  description: Erro ao deletar usuário
- *              404:
- *                  description: Usuário não encontrado
- */
 router.delete('/deletar_usuario/:id', async (req, res) => {
     try {
         await Usuario.findByIdAndDelete(req.params.id);
